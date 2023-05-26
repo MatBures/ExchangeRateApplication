d3eff72ec3ad1610391939f33ded0818bacd44e7
Lusignan <mat.bures@gmail.com>
2023-05-26 12:09:16 +0200

    Repository code updated

--- src/main/java/com/matyasbures/exchangerateproject/ExchangeRateRepository.java
+++ src/main/java/com/matyasbures/exchangerateproject/ExchangeRateRepository.java
@@ -1,11 +1,13 @@
 package com.matyasbures.exchangerateproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * This is an interface that extends JpaRepository, which provides generic CRUD
  * operations for a given model (in this case, ExchangeRateModel) and ID type (Long).
  */
+@Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRateModel, Long> {
 
 }
